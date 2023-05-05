ed2f95cb8f814424697c5ab308ed4fd4ba534f27
YuliiaKulyk13 <kulyk.yuliia13@gmail.com>
2023-05-05 14:25:30 -0400

    refactor code to redux

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,10 +1,13 @@
 import { useState } from 'react';
-
+import { nanoid } from 'nanoid';
 import { FiSearch } from 'react-icons/fi';
 import { FormBtn, InputSearch, SearchFormStyled } from './SearchForm.styled';
+import { useDispatch } from 'react-redux';
+import { addTodo } from 'redux/todoSlice';
 
 export const SearchForm = ({ onSubmit }) => {
   const [query, setQuery] = useState('');
+  const dispatch = useDispatch();
 
   const handleInput = e => {
     setQuery(e.currentTarget.value);
@@ -12,8 +15,11 @@ export const SearchForm = ({ onSubmit }) => {
 
   const handleSubmit = e => {
     e.preventDefault();
-
-    onSubmit(query);
+    const todo = {
+      id: nanoid(),
+      text: query,
+    };
+    dispatch(addTodo(todo));
     setQuery('');
   };
 
